4e33a7d24e268bee2c48ee7ecb3196ca1cc981cd
Hildur Hanna Lonn Holmqvist <hildurhanna.lonnholmqvist@gmail.com>
2023-08-07 17:09:50 +0200

    Adds gitignore ue5

---
+++ exercises/Source/exercises/exercisesGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "exercisesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EXERCISES_API AexercisesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};

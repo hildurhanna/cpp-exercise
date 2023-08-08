4d4a96c015b2a8d617a06433041d970ac35114f7
Hildur Hanna Lonn Holmqvist <hildurhanna.lonnholmqvist@gmail.com>
2023-08-08 11:27:20 +0200

    creates rotator cpp

---
+++ exercises/Source/exercises/RotatorA.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RotatorA.h"
+
+// Sets default values
+ARotatorA::ARotatorA()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ARotatorA::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ARotatorA::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+

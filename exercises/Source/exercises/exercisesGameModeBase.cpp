3086f1ebc08971fb3091b3d7c50046a8827518a0
Hildur Hanna Lonn Holmqvist <hildurhanna.lonnholmqvist@gmail.com>
2023-08-09 11:45:48 +0200

    Adds logic to game mode

--- exercises/Source/exercises/exercisesGameModeBase.cpp
+++ exercises/Source/exercises/exercisesGameModeBase.cpp
@@ -3,3 +3,16 @@
 
 #include "exercisesGameModeBase.h"
 
+void AexercisesGameModeBase::InitGame(const FString& MapName, const FString& Options, FString& ErrorMessage) 
+{
+	Super::InitGame(MapName, Options, ErrorMessage);
+	StatueManager = GetWorld()->SpawnActor<AStatueManager>();
+	StatueManager->Initialize(StatueClass);
+
+}
+
+void AexercisesGameModeBase::SpawnStatue(const FVector Location)
+{
+	StatueManager->SpawnStatue(Location);
+}
+
--- exercises/Source/exercises/exercisesGameModeBase.h
+++ exercises/Source/exercises/exercisesGameModeBase.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "StatueManager.h"
+#include "Statue.h"
 #include "exercisesGameModeBase.generated.h"
 
 /**
@@ -13,5 +15,18 @@ UCLASS()
 class EXERCISES_API AexercisesGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
+
+private:
+	UPROPERTY()
+		AStatueManager* StatueManager;
+
+public:
+	UPROPERTY(BlueprintReadWrite, EditAnywhere)
+		TSoftClassPtr<AStatue> StatueClass;
+
+	virtual void InitGame(const FString& MapName, const FString& Options, FString& ErrorMessage) override;
+
+	UFUNCTION()
+		void SpawnStatue(const FVector Location);
 	
 };

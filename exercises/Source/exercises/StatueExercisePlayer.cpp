2e6f1020c85bcbce88329f8ab99a9d0a72bcc3f9
Hildur Hanna Lonn Holmqvist <hildurhanna.lonnholmqvist@gmail.com>
2023-08-14 10:30:14 +0200

    Adds logic to player

--- exercises/Source/exercises/StatueExercisePlayer.cpp
+++ exercises/Source/exercises/StatueExercisePlayer.cpp
@@ -2,6 +2,7 @@
 
 
 #include "StatueExercisePlayer.h"
+#include "Kismet/KismetMathLibrary.h"
 
 // Sets default values
 AStatueExercisePlayer::AStatueExercisePlayer()
@@ -9,26 +10,47 @@ AStatueExercisePlayer::AStatueExercisePlayer()
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	CapsuleComponent = CreateDefaultSubobject<UCapsuleComponent>(TEXT("CapsuleCollider"));
+	CapsuleComponent->InitCapsuleSize(55.f, 96.f);
+	CapsuleComponent->SetSimulatePhysics(true);
+
+	constexpr float InertiaTensorScale = 10000.f;
+	CapsuleComponent->BodyInstance.InertiaTensorScale = FVector(InertiaTensorScale, InertiaTensorScale, InertiaTensorScale);
+
+	SetRootComponent(CapsuleComponent);
+
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	CameraComponent->SetupAttachment(CapsuleComponent);
+	CameraComponent->SetRelativeLocation(FVector(0, 0, 100.f));
+
 }
 
-// Called when the game starts or when spawned
-void AStatueExercisePlayer::BeginPlay()
+void AStatueExercisePlayer::Look(const FVector2D AxisInput)
 {
-	Super::BeginPlay();
+	//Rotate on the horizontal axis
+	AddActorLocalRotation(FRotator(0.f, AxisInput.X, 0.f));
+
+	//Clamp vertical axis
+	CurrentLookRotation += AxisInput.Y;
+	CurrentLookRotation = FMath::ClampAngle(CurrentLookRotation, -90.f, 90.f);
+
+	CameraComponent->SetWorldRotation(FRotator(CurrentLookRotation, GetActorRotation().Yaw, 0.f));
+
+}
+
+void AStatueExercisePlayer::Move(const FVector2D AxisInput) 
+{
+	auto moveVector = FVector(AxisInput.Y, -AxisInput.X, 0.f);
+
+	auto transformedVector = UKismetMathLibrary::TransformDirection(GetActorTransform(), moveVector);
+
+	auto deltaTime = GetWorld()->GetDeltaSeconds();
+
+	transformedVector *= moveSpeed;
+
+	SetActorLocation(
+		GetActorLocation() +
+		moveVector * deltaTime
+	);
 	
 }
-
-// Called every frame
-void AStatueExercisePlayer::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
-// Called to bind functionality to input
-void AStatueExercisePlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-}
-
--- exercises/Source/exercises/StatueExercisePlayer.h
+++ exercises/Source/exercises/StatueExercisePlayer.h
@@ -4,6 +4,8 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
 #include "StatueExercisePlayer.generated.h"
 
 UCLASS()
@@ -11,19 +13,31 @@ class EXERCISES_API AStatueExercisePlayer : public APawn
 {
 	GENERATED_BODY()
 
+private:
+	float CurrentLookRotation;
+
+
+protected:
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+		UCameraComponent* CameraComponent;
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+		UCapsuleComponent* CapsuleComponent;
+
 public:
 	// Sets default values for this pawn's properties
 	AStatueExercisePlayer();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	UPROPERTY(BlueprintReadOnly, EditAnywhere)
+		float moveSpeed = 1000.f;
+	UFUNCTION(BlueprintCallable)
+		void Move(const FVector2D AxisInput);
+	UFUNCTION(BlueprintCallable)
+		void Look(const FVector2D AxisInput);
+	UFUNCTION(BlueprintCallable)
+	const FTransform CameraTransform()
+	{
+		return CameraComponent->GetComponentTransform();
+	}
 
 };
